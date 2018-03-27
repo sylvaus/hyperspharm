59be867ef8523f53ddddd9c5edb6cb558622558c
Sylvaus <pierreyves.breches74@gmail.com>
2018-03-26 21:51:08 -0400

    Implemented Spharm Containers

--- include/spharms.h
+++ include/spharms.h
@@ -26,44 +26,34 @@ namespace hyperspharm
 class SphericalSurface
 {
 public:
-  SphericalSurface(const natural_t nrows, const natural_t ncols);
+  SphericalSurface(const natural_t rows, const natural_t cols);
   
-  std::vector<real_t>& operator[](const int index );
-  
-  natural_t nrows();
-  natural_t ncols();
-  
-  void changeNorthPole(const real_t value);
-  void changeSouthPole(const real_t value);
+  real_t get(const natural_t theta_n, const natural_t psi_m);
+  void set(const natural_t theta_n, const natural_t psi_m, const real_t radius_nm);
+
+  const natural_t rows;
+  const natural_t cols;
 
 private:
-  natural_t nrows_;
-  natural_t ncols_;
-  std::vector<std::vector<real_t>> values_;
+  std::vector<real_t> values_;
 };
 
 class SphericalHarmonics
 {
-  SphericalHarmonics(const natural_t nmax, const natural_t mmax);
-  
-  std::vector<real_t>& operator[](const natural_t index );
-  
-  natural_t nmax();
-  natural_t mmax();
+  explicit SphericalHarmonics(const natural_t l_max);
+
+  complex_t get(const natural_t l, const natural_t m);
+  void set(const natural_t l, const natural_t m, const complex_t value);
+
+  const natural_t l_max;
   
 private:
-  natural_t nmax_;
-  natural_t mmax_;
   std::vector<complex_t> values_;
 };
 
 class Spharm
 {
 public:
-  static void compute_clmk(const natural_t l_max);
-  
-  
-  
   static SphericalHarmonics spharm_transform(const SphericalSurface& spherical_surface);
   static SphericalSurface ispharm_transform(const SphericalHarmonics& spherical_harmonics);
 };
--- src/spharms.cpp
+++ src/spharms.cpp
@@ -11,6 +11,48 @@
 
 namespace hyperspharm
 {
-  
 
+
+SphericalSurface::SphericalSurface(const natural_t rows, const natural_t cols) :
+  rows(rows), cols(cols), values_(rows * cols)
+{
+}
+
+real_t SphericalSurface::get(natural_t theta_n, const natural_t psi_m)
+{
+  return values_[rows * theta_n + psi_m];
+}
+
+void SphericalSurface::set(const natural_t theta_n, const natural_t psi_m, const real_t radius_nm)
+{
+  values_[rows * theta_n + psi_m] = radius_nm;
+}
+
+
+
+SphericalHarmonics::SphericalHarmonics(const natural_t l_max) :
+  l_max(l_max), values_(((l_max + 2)*(l_max + 1))/2)
+{
+}
+
+complex_t SphericalHarmonics::get(const natural_t l, const natural_t m)
+{
+  const size_t index = ((l+1) * l)/2 + m;
+  if(index >= values_.size())
+  {
+    return {0, 0};
+  }
+
+  return values_[index];
+}
+
+void SphericalHarmonics::set(const natural_t l, const natural_t m, const complex_t value)
+{
+  const size_t index = ((l+1) * l)/2 + m;
+  if(index >= values_.size())
+  {
+    return;
+  }
+  values_[index] = value;
+}
 }
