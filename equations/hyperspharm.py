2e2de9cee46bd859af5e164f1b165f91760f5018
sylvaus <pierreyves.breches74@gmail.com>
2018-10-20 10:17:46 -0400

    Began writing hyperspharm transform

--- equations/hyperspharm.py
+++ equations/hyperspharm.py
@@ -1,4 +1,4 @@
-from sympy import sin, cos, Function, Symbol, exp, Pow, sqrt, pi, factorial
+from sympy import sin, cos, Function, exp, Pow, Integral, pi
 from sympy.abc import l, theta, phi, beta, i, m, x
 
 from print_helpers import print_equation
@@ -14,8 +14,11 @@ def NGml(m_, l_, Gmlx):
     return gg.norm_coeff(m_, l_) * Gmlx
 
 
+def integral_over_hyper_sphere(func):
+    return Integral(Integral(Integral(func, (beta, 0, pi)),(theta, 0, pi)), (phi, 0, 2*pi))
+
 def hyperspharm():
-    Znlm_name = Function("Z__n_l,m")
+    Znlm_name = Function("Z__m_l,n")
     NPml_name = Function("NP__m_l")
     NG_l_1_n_l_name = Function("NG__l+1_n-l")
     print_equation(Znlm_name(beta, theta, phi),
@@ -25,5 +28,19 @@ def hyperspharm():
     print_equation(Function("NG__m_l")(x), NGml(m, l, Function("G__m_l")(x)), True)
 
 
+def hyperspharm_transform():
+    Znlm_name = Function("Z__m*_l,n")
+    fnlm = Function("f__m_l,n")
+    f_func = Function("f")
+    print_equation(
+        fnlm,
+        integral_over_hyper_sphere(f_func(beta, theta, phi) *
+                                   Znlm_name(beta, theta, phi) *
+                                   sin(beta) ** 2 * sin(theta)),
+        True
+    )
+
+
 if __name__ == '__main__':
     hyperspharm()
+    hyperspharm_transform()
